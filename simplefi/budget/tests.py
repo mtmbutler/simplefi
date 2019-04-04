08d2d587889b73893804a8af83be4330144ecb56
Miles Butler <mtmbutler@icloud.com>
2019-04-03 20:04:17 -0700

    Add django-pytest and an initial test

--- simplefi/budget/tests.py
+++ simplefi/budget/tests.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from .models import Statement
+
+
+def test_statement_date():
+    s = Statement(account=None, year=2000, month=1, balance=0.00)
+    assert s.date == datetime.date(year=2000, month=1, day=1)
