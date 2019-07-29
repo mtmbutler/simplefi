f19478df36935c2b0bef407b69828fbebff0df4a
Miles Butler <mtmbutler@icloud.com>
2019-07-29 07:47:04 -0700

    Deprecate config file, use environment instead
     - Clean up settings file for Heroku use

--- scripts/backup_db.py
+++ scripts/backup_db.py
@@ -5,25 +5,11 @@ import os
 import subprocess
 import sys
 
-import yaml
-
 # Define paths
-repo_dir = os.path.join(
-    os.path.dirname(__file__),
-    os.pardir)
-conf_path = os.path.join(
-    repo_dir,
-    'config.yaml')
-manage_path = os.path.join(
-    repo_dir,
-    'simplefi',
-    'manage.py')
-
-# Get dump path from config
-with open(conf_path) as f:
-    dump_dir = yaml.safe_load(f)['BACKUP_PATH']
+repo_dir = os.path.join(os.path.dirname(__file__), os.pardir)
+manage_path = os.path.join(repo_dir, 'manage.py')
 datestr = datetime.datetime.now().strftime("%y%m%d_%H%M%S")
-out_path = os.path.join(dump_dir, f'data_{datestr}.json')
+out_path = os.path.join(repo_dir, 'fixtures', f'data_{datestr}.json')
 
 # Run django-admin's dumpdata with proper args
 subprocess.call([
